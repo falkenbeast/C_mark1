c84e8207e8363854a26a92603aba4a670bc10b79
falkenbeast <arshayush21@gmail.com>
2023-10-06 10:14:36 +0530

    changes

--- ch8_p1.c
+++ ch8_p1.c
@@ -14,7 +14,8 @@ int main()
 {
     // char str[] = {'h', 'a', 'r', 'r', 'y', '\0'};
     // char str[6] = "harry";
-    char str[34];
+   // char str[3];
+   char str[0]; //the value given inside the brackets is just to initialize the array 
     gets(str);
     printf("Using custom function printStr\n");
     printStr(str);
